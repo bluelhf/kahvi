7ac0cc5c05065851e37bb2c5213f0ec3f348ec6d
Blue <blueloveshisfriends@gmail.com>
2021-09-30 15:01:59 +0300

    Add welcome to CLI

--- src/main/java/blue/lhf/kahvi/cli/KahviCLI.java
+++ src/main/java/blue/lhf/kahvi/cli/KahviCLI.java
@@ -1,15 +1,21 @@
 package blue.lhf.kahvi.cli;
 
+import blue.lhf.kahvi.util.ProjectUtils;
+
 public class KahviCLI {
     public static void main(String[] args) {
         KahviCLI cli = new KahviCLI();
+        cli.welcome();
     }
 
     public void welcome() {
         System.out.printf("""
                 welcome to kahvi!
                    java-based interpreted language
-                loading kahvi-cli
-                """);
+                loading kahvi-cli v%s
+                """, ProjectUtils.getManifest()
+                .getMainAttributes()
+                .getValue("Version")
+        );
     }
 }
