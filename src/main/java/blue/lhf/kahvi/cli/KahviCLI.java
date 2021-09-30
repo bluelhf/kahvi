1fbf9b0e7bf22e6e11d012147f66d2058196de23
Blue <blueloveshisfriends@gmail.com>
2021-09-30 14:44:56 +0300

    Initial commit

---
+++ src/main/java/blue/lhf/kahvi/cli/KahviCLI.java
@@ -0,0 +1,15 @@
+package blue.lhf.kahvi.cli;
+
+public class KahviCLI {
+    public static void main(String[] args) {
+        KahviCLI cli = new KahviCLI();
+    }
+
+    public void welcome() {
+        System.out.printf("""
+                welcome to kahvi!
+                   java-based interpreted language
+                loading kahvi-cli v%s
+                """, );
+    }
+}
